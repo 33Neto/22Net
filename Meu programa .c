59c2cb76887144d845aa0e16349b6323284db9f8
33Neto <97692627+33Neto@users.noreply.github.com>
2022-01-13 23:58:24 -0400

    Update Meu programa .c

--- Meu programa .c
+++ Meu programa .c
@@ -2,12 +2,32 @@
 
 int main ()
 {
+	int digite;
 	
-       int suaIdade;
-
-       printf ("Digite sua idade");
-       scanf ("%i", &suaIdade);
-       
-       return 0; 
-       
+	printf ("BEM-VINDO A MINHA FERRAMENTA.\n");
+	
+    printf ("-----------------------------\n");
+	
+	printf ("Selecione algum numero abaixo?\n");
+	printf ("\n1 location\n");
+	printf ("2 Como usar\n");
+	printf ("3 GPS\n");
+	
+	printf ("\n");
+	printf ("\n");
+	
+	scanf("%i", &digite);
+	printf ("Aguarde.......");
+	
+	printf ("\n");
+	printf ("\n");
+	printf ("\n");
+		
+	printf ("Criador: MAMAO PAPAYE");
+	
+	             
+    return 0;
+    
+    
 }
+
